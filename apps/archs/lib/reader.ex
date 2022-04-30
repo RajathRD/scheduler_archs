b90255aef07f6f29beabf50f6b40b79e2df7a7cc
Amit Porje <52151309+amit4j@users.noreply.github.com>
2022-04-30 17:19:43 -0400

    Enhance reader functionality
    Add support for scaling, trace path and scheduler config
    Send message to scheduler
    Test to run centralized scheduler with jobs read from csv

--- apps/archs/lib/reader.ex
+++ apps/archs/lib/reader.ex
@@ -1,60 +1,125 @@
+defmodule Reader.Config do
+  @enforce_keys []
+  defstruct(
+    scaling_factor_time: 1,
+    scaling_factor_cpu: 10000,
+    scaling_factor_mem: 15000,
+    trace_path: "resources/trace.csv",
+    schedulers: []
+  )
+end
+
 defmodule Reader do
-  import Kernel, except: [if: 2, unless: 2]
+
+  import Kernel, except: [if: 2, unless: 2, send: 2]
+  import Emulation, only: [send: 2, whoami: 0]
 
   @moduledoc false
 
-  #TODOS -
-  # Filter and publish everything that arrives at time 0 first.
-  # Post this, track system time and send message to scheduler with details when arrival_time >= current_time - start_time. Else wait.
-  # To accomodate time scaling, just use a factor to divide arrival time and then publish events as needed.
-  def read do
-    File.stream!('resources/trace.csv')
+  require Logger
+
+  def process(row, start_time, config) do
+    y = elem(row, 1)
+    scaling_factor_cpu = config.scaling_factor_cpu
+    scaling_factor_mem = config.scaling_factor_mem
+    scaling_factor_time = config.scaling_factor_time
+    IO.inspect(y)
+
+    job_id = Enum.fetch(y, 0)
+             |> elem(1)
+             |> Integer.parse
+             |> elem(0)
+
+    task_id = Enum.fetch(y, 1)
+              |> elem(1)
+              |> Integer.parse
+              |> elem(0)
+
+    arrival_time = Enum.fetch(y, 2)
+                   |> elem(1)
+                   |> Integer.parse
+                   |> elem(0)
+                   |> Kernel./(1000) # microsecond to millisecond
+                   |> Kernel./(scaling_factor_time)
+                   |> trunc
+
+    cpus = (scaling_factor_cpu * (Enum.fetch(y, 3)
+                                  |> elem(1)
+                                  |> Float.parse
+                                  |> elem(0)))
+           |> trunc
+
+    memory = (scaling_factor_mem * (Enum.fetch(y, 4)
+                                    |> elem(1)
+                                    |> Float.parse
+                                    |> elem(0)))
+             |> trunc
+
+    run_time = Enum.fetch(y, 5)
+               |> elem(1)
+               |> Integer.parse
+               |> elem(0)
+               |> Kernel./(1000) # microsecond to millisecond
+               |> Kernel./(scaling_factor_time)
+               |> trunc
+
+
+    payload = Job.Payload.new("", job_id, task_id, arrival_time, run_time, cpus, memory)
+
+    delay = _get_delay(start_time, arrival_time)
+    IO.puts("delay #{delay}")
+    #    If delay is too great, hold off processing for a bit
+
+    #    if delay > 30000 do
+    #      IO.puts("Delay is too large, waiting for a while")
+    #      Process.sleep(delay - 25000)
+    #      delay = _get_delay(start_time, arrival_time)
+    #    end
+
+    submit(payload, delay, config.schedulers)
+  end
+
+  def _get_delay(start_time, arrival_time) do
+    system_time = DateTime.utc_now()
+                  |> DateTime.to_unix(:millisecond)
+    current_exec_time = system_time - start_time
+    delay = max(arrival_time - current_exec_time, 0)
+            |> trunc
+    delay
+  end
+
+  def submit(payload, delay, schedulers) do
+    spawn(
+      fn ->
+        pid = whoami()
+
+        Logger.debug("Process #{pid}, adding delay of #{delay} to payload #{payload}")
+
+        Process.sleep(delay)
+
+        Logger.debug("Process #{pid}, woke up after delay #{delay}, sending #{payload}")
+
+        sch = Enum.random(schedulers)
+        payload = %{payload | scheduler: sch, client: pid}
+        IO.inspect(payload)
+        send(sch, {:job_submit, payload})
+      end
+    )
+  end
+
+  def read(config) do
+    IO.puts("Start reading")
+    start_time = DateTime.utc_now()
+                 |> DateTime.to_unix(:millisecond)
+
+    File.stream!(config.trace_path)
     |> CSV.decode
-      # Remove ->
-    |> Enum.take(1)
+#    |> Enum.take(1000)
     |> Stream.each(
-         fn x ->
-           y = elem(x, 1)
-           scaling_factor_cpu = 10000
-           scaling_factor_mem = 15000
-
-           job_id = Enum.fetch(y, 0)
-                    |> elem(1)
-                    |> Integer.parse
-                    |> elem(0)
-
-           task_id = Enum.fetch(y, 1)
-                     |> elem(1)
-                     |> Integer.parse
-                     |> elem(0)
-
-           arrival_time = Enum.fetch(y, 2)
-                          |> elem(1)
-                          |> Integer.parse
-                          |> elem(0)
-
-           cpus = (scaling_factor_cpu * (Enum.fetch(y, 3)
-                                         |> elem(1)
-                                         |> Float.parse
-                                         |> elem(0)))
-                  |> trunc
-
-           memory = (scaling_factor_mem * (Enum.fetch(y, 4)
-                                           |> elem(1)
-                                           |> Float.parse
-                                           |> elem(0)))
-                    |> trunc
-
-           run_time = Enum.fetch(y, 5)
-                      |> elem(1)
-                      |> Integer.parse
-                      |> elem(0)
-
-           IO.puts("#{job_id} - #{task_id} - #{arrival_time} - #{cpus} - #{memory} - #{run_time}")
-
+         fn item ->
+           process(item, start_time, config)
          end
        )
     |> Stream.run
   end
-
 end
--- apps/archs/test/scheduler_test.exs
+++ apps/archs/test/scheduler_test.exs
@@ -6,9 +6,9 @@ defmodule SCHTest do
   doctest Cluster.Node
 
   import Emulation, only: [spawn: 2]
-
+  import Reader
   import Kernel,
-    except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
+         except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
 
   test "Run Centralized Scheduler" do
     Emulation.init()
@@ -70,4 +70,29 @@ defmodule SCHTest do
   # after
   #   Emulation.terminate()
   # end
+
+  test "Run Centralized Scheduler with Reader" do
+    Emulation.init()
+    cluster_config = Cluster.Config.default()
+
+    cluster = Cluster.setup(cluster_config)
+    spawn(:s_1, fn -> Scheduler.start(cluster) end)
+
+    reader_config = %Reader.Config{
+      scaling_factor_cpu: 100,
+      scaling_factor_mem: 150,
+      scaling_factor_time: 1,
+      schedulers: [:s_1],
+      #TODO: Handle better
+      trace_path: "#{Path.expand("../../../", __DIR__)}/resources/trace.csv"
+    }
+    Reader.read(reader_config)
+    Process.send_after(self(), :timeout, 2_000)
+    # Timeout.
+    receive do
+      :timeout -> assert true
+    end
+  after
+    Emulation.terminate()
+  end
 end
